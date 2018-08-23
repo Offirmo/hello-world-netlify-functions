48431ffb6c840446ba94215299a949a47581d068
Offirmo <offirmo.net@gmail.com>
2018-08-23 15:15:58 +1000

    TLDW :scream_cat:

--- src/echo.js
+++ src/echo.js
@@ -1,13 +1,11 @@
 // see https://www.netlify.com/docs/functions/
 
 exports.handler = function(event, context, callback) {
-	const all_the_things = `<pre>
-${JSON.stringify({
+	const all_the_things = JSON.stringify({
 		context,
 		event,
 		env: process.env,
-}, null, 2)}
-</pre>`
+	}, null, 2)
 
 	console.log(all_the_things)
 
